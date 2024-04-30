79fa41e6869736eb7253602190f5de6c1a2fbfea
BuildTools <unconfigured@null.spigotmc.org>
2024-04-30 18:13:09 -0400

    First Commit

---
+++ src/main/java/net/foi1y/seakings/SeaKingsModClient.java
@@ -0,0 +1,10 @@
+package net.foi1y.seakings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SeaKingsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/foi1y/seakings/SeaKingsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.foi1y.seakings;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SeaKingsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
