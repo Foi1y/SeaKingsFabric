8ae22653e03118410d751dafa1e33f3562fe16b5
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-18 15:27:09 +1000

    dedecated server entrypoint

--- src/main/java/net/foi1y/seakings/SeaKingsModDataGenerator.java
+++ src/main/java/net/foi1y/seakings/SeaKingsModDataGenerator.java
@@ -1,11 +1,9 @@
 package net.foi1y.seakings;
 
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.*;
 
 public class SeaKingsModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
 	}
 }
---
+++ src/main/java/net/foi1y/seakings/SeaKingsModServer.java
@@ -0,0 +1,9 @@
+package net.foi1y.seakings;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class SeaKingsModServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+    }
+}
