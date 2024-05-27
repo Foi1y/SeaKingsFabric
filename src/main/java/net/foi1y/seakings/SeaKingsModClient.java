4362ec7345e8867f29ef1bf0cfca5ea9026b1c60
Cyclic <cyclic007dev@gmail.com>
2024-05-27 03:09:44 -0500

    Back to the wheel of DOOM was meant to do

--- src/main/java/net/foi1y/seakings/SeaKingsModClient.java
+++ src/main/java/net/foi1y/seakings/SeaKingsModClient.java
@@ -6,37 +6,49 @@ import dev.emi.trinkets.api.client.TrinketRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.foi1y.seakings.client.screens.ExampleGUI;
 import net.foi1y.seakings.client.screens.ExampleScreen;
-import net.foi1y.seakings.client.screens.MenuScreen;
 import net.foi1y.seakings.item.ModItems;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class SeaKingsModClient implements ClientModInitializer {
-    private static KeyBinding keyBinding;
+    private static KeyBinding openMenu;
+    private static KeyBinding showWheel;
     @Override
     public void onInitializeClient() {
         TrinketRendererRegistry.registerRenderer(ModItems.WHITE_CLOAK, (TrinketRenderer) ModItems.WHITE_CLOAK);
         TrinketRendererRegistry.registerRenderer(ModItems.BLUE_CLOAK, (TrinketRenderer) ModItems.BLUE_CLOAK);
 
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openMenu = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.seakings.menu", // The translation key of the keybinding's name
                 InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
                 GLFW.GLFW_KEY_R, // The keycode of the key
                 "category.seakings.keys" // The translation key of the keybinding's category.
         ));
 
+        showWheel = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.seakings.wheel",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_GRAVE_ACCENT,
+                "category.seakings.keys"
+        ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
+            while (openMenu.wasPressed()) {
                 assert client.player != null;
                 client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
-                MinecraftClient.getInstance().setScreen(new ExampleScreen(new ExampleGUI(keyBinding)));
+                MinecraftClient.getInstance().setScreen(new ExampleScreen(new ExampleGUI(openMenu)));
+            }
+        });
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (showWheel.isPressed()) {
+                assert client.player != null;
+                client.player.sendMessage(Text.literal("Filler text"),true);
+
             }
         });
     }
---
+++ src/main/java/net/foi1y/seakings/client/screens/theWheelOfDoom.java
@@ -0,0 +1,16 @@
+package net.foi1y.seakings.client.screens;
+
+import io.github.cottonmc.cotton.gui.client.CottonHud;
+import io.github.cottonmc.cotton.gui.widget.WWidget;
+
+public class theWheelOfDoom implements CottonHud.Positioner {
+
+    public theWheelOfDoom() {
+
+    }
+
+    @Override
+    public void reposition(WWidget widget, int hudWidth, int hudHeight) {
+
+    }
+}
