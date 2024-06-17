64e46e1a0d4bbfed18c85c05a59d997abadd8451
BuildTools <unconfigured@null.spigotmc.org>
2024-06-17 15:09:22 -0400

    Added commands.txt

--- src/main/java/net/foi1y/seakings/SeaKingsModServer.java
+++ src/main/java/net/foi1y/seakings/SeaKingsModServer.java
@@ -5,9 +5,6 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class SeaKingsModServer implements DedicatedServerModInitializer {
 
-    // Commands for Future Use
-    // /scoreboard objectives add Bounty dummy "[Bounty]"
-    // /scoreboard objectives add StyleMastery dummy "[Style Mastery]"
     @Override
     public void onInitializeServer() {
     }
